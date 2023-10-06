7bb5a57727b5f919469396fddc0625fd32737a00
kamilakhtar015 <143154996+kamilakhtar015@users.noreply.github.com>
2023-10-06 22:27:13 +0500

    Update userController.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -5,9 +5,38 @@ const jwt = require('jsonwebtoken')
 
 const SECRET_KEY = "NOTEAPIS"
 
+
+const passwordChecks = (password) => {
+    if (password.length < 8 || password.length > 128) {
+        return "Password should be between 8 and 128 characters.";
+    }
+    if (!/[A-Z]/.test(password)) {
+        return "Password should contain at least one uppercase letter.";
+    }
+    if (!/[a-z]/.test(password)) {
+        return "Password should contain at least one lowercase letter.";
+    }
+    if (!/[0-9]/.test(password)) {
+        return "Password should contain at least one digit.";
+    }
+    if (!/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/.test(password)) {
+        return "Password should contain at least one special character.";
+    }
+    return null;
+};
+
+
 const signup = async (req, res) => {
 
     const { username, email, password } = req.body;
+
+    
+    // Password Validation
+    const passwordError = passwordChecks(password);
+    if (passwordError) {
+        return res.status(400).json({ message: passwordError });
+    }
+
     try {
 
         // checking for existing users
@@ -16,6 +45,7 @@ const signup = async (req, res) => {
             return res.status(400).json({ message: "User already exists" });
         }
 
+        // Password Hashingd
         const hashedPassword = await bcrypt.hash(password, 10);
 
         // create a new user
